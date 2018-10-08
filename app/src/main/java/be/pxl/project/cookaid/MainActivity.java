28c48892db5e32dc5a5867d6cf63065cce12ad09
Cedricmarx <marxcedric@hotmail.com>
2018-10-08 16:26:04 +0200

    create the activity_login.xml and activity_register.xml

--- app/src/main/java/be/pxl/project/cookaid/MainActivity.java
+++ app/src/main/java/be/pxl/project/cookaid/MainActivity.java
@@ -1,5 +1,6 @@
 package be.pxl.project.cookaid;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,19 +14,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         View decorView = getWindow().getDecorView();
-// Hide both the navigation bar and the status bar.
-// SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
-// a general rule, you should design your app to hide the status bar whenever you
-// hide the navigation bar.
+        // Hide both the navigation bar and the status bar.
+        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+        // a general rule, you should design your app to hide the status bar whenever you
+        // hide the navigation bar.
         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
-
 
         setContentView(R.layout.activity_main);
 
         TextView tx = findViewById(R.id.logoTextView);
         Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/Lovelo_Black.otf");
         tx.setTypeface(custom_font);
+    }
+
+    public void loginButton_Clicked(View view) {
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+    }
+
+    public void signUpButton_Clicked(View view) {
+        startActivity(new Intent(MainActivity.this, RegisterActivity.class));
     }
 }
